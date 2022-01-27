b0d4c2222a12b44be7ca90ceee08b0d46dff00e6
Andres Gonzalez Espinosa <andres@Andress-MBP.lan>
2022-01-27 12:00:19 +0100

    primer commit

---
+++ pages/about.jsx
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+  return (
+    <Layout title="About | next.js" description="agruegue una descripcion">
+        <h1>About</h1>
+    </Layout>);
+}
